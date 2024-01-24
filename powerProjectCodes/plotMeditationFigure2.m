3cd29579f9d6e3292084a2ce50b49956af656222
Ankan Biswas <ankanbiswas022@gmail.com>
2024-01-24 18:08:03 +0530

    Minor modification to figure 2 to combine data for common protocols

--- powerProjectCodes/plotMeditationFigure2.m
+++ powerProjectCodes/plotMeditationFigure2.m
@@ -9,13 +9,18 @@ fontsize = 12;
 comparisonStr = 'paired';
 % % protocolNames = {'G2'}; refChoices = {'none'};
 
-protocolNames = [{'EO1'}  {'EC1'}  {'G1'}];
-refChoices    = [{'none'} {'none'} {'none'}];
+% combineDataAcrossCommonProtocols = 0;
 
-% analysisChoice = 'st';
-analysisChoice = {'combined','combined','bl'};
-% analysisMode   = 'combineData';
-combineDataAcrossCommonProtocols = 0;
+% protocolNames = [{'EO1'}  {'EC1'}  {'G1'} ];
+% refChoices    = [{'none'} {'none'} {'none'}] ;
+% analysisChoice = {'combined','combined','bl'};
+
+combineDataAcrossCommonProtocols = 1;
+
+combIndex      = {[1,4],[2,5],[3,6]};
+protocolNames  = [{'EO1'}  {'EC1'}  {'G1'} {'EO2'}  {'EC2'}  {'G2'}];
+refChoices     = [{'none'} {'none'} {'none'} {'none'}  {'none'}  {'none'}] ;
+analysisChoice = {'combined','combined','bl','combined','combined','bl'};
 
 badEyeCondition = 'ep';
 badTrialVersion = 'v8';
@@ -86,54 +91,52 @@ else % either combine or just get the data
     end
 
     if combineDataAcrossCommonProtocols         % Average data across protocols
-        logPSDData = cell(numGroups,2);
-        logPower = cell(numGroups,2);
-        for g=1:numGroups
-            for i=1:2 % meditator/control
-                % Get subjects for each protocol
-                subjectNamesTMP = cell(1,numProtocols);
-                for j=1:numProtocols
-                    subjectNamesTMP{j} = goodSubjectNameListsTMP{j}{groupPos,i};
-                end
+        % Average data across protocols
+        logPSDData = cell(1,2);
+        logPower = cell(1,2);
 
-                % Get common subjects
-                commonSubjects = subjectNamesTMP{1};
-                for j=2:numProtocols
-                    commonSubjects = intersect(commonSubjects,subjectNamesTMP{j},'stable');
-                end
+        for i=1:2 % meditator/control
 
-                % Generate average data across protocols for each common subject
-                numCommonSubjects = length(commonSubjects);
-
-                logPSDCommon = zeros(numCommonSubjects,length(freqVals));
-                logPowerCommon = zeros(1,numCommonSubjects);
-                for j=1:numCommonSubjects
-                    name = commonSubjects{j};
-
-                    psdTMP=[]; powerTMP=[];
-
-                    for k=1:numProtocols
-                        pos = find(strcmp(name,subjectNamesTMP{k}));
-                        psdTMP = cat(3,psdTMP,logPSDDataTMP{g,k}{i}(pos,:));
-                        powerTMP = cat(2,powerTMP,logPowerTMP{g,k}{i}(pos));
-                    end
-
-                    logPSDCommon(j,:) = squeeze(mean(psdTMP,3));
-                    logPowerCommon(j) = mean(powerTMP);
-                end
-
-                logPSDData{i} = logPSDCommon;
-                logPower{i} = logPowerCommon;
+            % Get subjects for each protocol
+            subjectNamesTMP = cell(1,numProtocols);
+            for j=1:numProtocols
+                subjectNamesTMP{j} = goodSubjectNameListsTMP{j}{groupPos,i};
             end
+
+            % Get common subjects
+            commonSubjects = subjectNamesTMP{1};
+            for j=2:numProtocols
+                commonSubjects = intersect(commonSubjects,subjectNamesTMP{j},'stable');
+            end
+
+            % Generate average data across protocols for each common subject
+            numCommonSubjects = length(commonSubjects);
+
+            logPSDCommon = zeros(numCommonSubjects,length(freqVals));
+            logPowerCommon = zeros(1,numCommonSubjects);
+            for j=1:numCommonSubjects
+                name = commonSubjects{j};
+
+                psdTMP=[]; powerTMP=[];
+                for k=1:numProtocols
+                    pos = find(strcmp(name,subjectNamesTMP{k}));
+                    psdTMP = cat(3,psdTMP,logPSDDataTMP{k}{i}(pos,:));
+                    powerTMP = cat(2,powerTMP,logPowerTMP{k}{i}(pos));
+                end
+
+                logPSDCommon(j,:) = squeeze(mean(psdTMP,3));
+                logPowerCommon(j) = mean(powerTMP);
+            end
+
+            logPSDData{i} = logPSDCommon;
+            logPower{i} = logPowerCommon;
         end
+
     else
         logPSDData = logPSDDataTMP;
         logPower = logPowerTMP;
     end
 end
-
-
-
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Plot Psd %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -185,4 +188,3 @@ set(findobj(gcf,'type','axes'),'box','off'...
     ,'xcolor',[0 0 0]...
     ,'ycolor',[0 0 0]...
     );
-
