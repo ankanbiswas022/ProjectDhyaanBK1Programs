56ba8767c38add1551ab2097ffefc59ca8ef8cc1
Ankan Biswas <ankanbiswas022@gmail.com>
2024-02-05 08:00:24 +0530

    Adding G2(st) and M1(combined) lines to the respective plots for the comparison

--- powerProjectCodes/plotMeditationFigure5.m
+++ powerProjectCodes/plotMeditationFigure5.m
@@ -10,8 +10,6 @@ comparisonStr = 'paired';
 % % protocolNames = {'G2'}; refChoices = {'none'};
 colormap jet
 
-combineDataAcrossCommonProtocols = 0;
-
 protocolNames  = [{'M2'}  {'M2'}  {'M2'} {'M1'} {'G2'}];
 refChoices     = [{'none'} {'none'} {'M2'}  {'none'} {'none'}] ;
 analysisChoice = {'bl','st','st','combined','bl'};
@@ -19,7 +17,6 @@ analysisChoice = {'bl','st','st','combined','bl'};
 groupNames = {'Meditators','Controls'};
 colorList  = [rgb('RoyalBlue');rgb('DarkCyan')];
 
-combineDataAcrossCommonProtocols = 0;
 plotTopoFlag = 1;
 
 % combIndex      = {[1,4],[2,5],[3,6]};
@@ -61,8 +58,6 @@ end
 hAllPlots = getPlotHandles(2,4,[0.1 0.1 0.85 0.8],0.05);
 hTopo     = hAllPlots(:,end);
 
-
-
 annotation('textbox',[.12 .65 .1 .2], 'String','Occipital','EdgeColor','none','FontWeight','bold','FontSize',18,'Rotation',90);
 annotation('textbox',[.12 .18 .1 .2], 'String','Fronto-Central','EdgeColor','none','FontWeight','bold','FontSize',18,'Rotation',90);
 
@@ -100,7 +95,6 @@ else % either combine or just get the data
     logTopoData = logTopoDataTMP;
 end
 
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Plot Psd %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 displaySettings.fontSizeLarge = 10;
@@ -122,57 +116,78 @@ x = load([capType '.mat']); montageChanlocs = x.chanlocs;
 
 for g=1:length(groupPos)
     for i=1:numProtocols
-        hPSD = hAllPlots(g,i);
-        displayAndcompareData(hPSD,logPSDData{g,i},freqVals,displaySettings,yLimsPSD,1,useMedianFlag,~pairedDataFlag);
-        xlim(hPSD,freqLims);
 
-        % Add lines in PSD plots
-        for k=1:2
-            line([freqRangeList{freqPos}(k) freqRangeList{freqPos}(k)],yLimsPSD,'LineStyle','--','LineWidth',2,'color','k','parent',hPSD);
-        end
+        %%%%%%%%%%%%%% Add M1(BL-combined line) or G2 (st) for comparision %%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%% to the M2-Bl (1st Panel) and  M2 St (Second Panel) (st) for comparision %%%%%%%%%%%%%%%%%%%
 
-        if ~strcmp(refChoices{1},'none')
-            line([0 freqVals(end)],[0 0],'color','k','parent',hPSD);
-        end
+        if i>3
+            switch i
+                case 4 % M1 combined
+                    p=4; % EO1
+                    hPSD = hAllPlots(g,1);
+                    showOnlyLineFlag = 1;
+                    lineStyle = '--';
+                    displayAndcompareData(hPSD,logPSDData{g,p},freqVals,displaySettings,yLimsPSD,0,useMedianFlag,~pairedDataFlag,showOnlyLineFlag,lineStyle);
+                case 5 % G2(st)
+                    p=5;
+                    hPSD = hAllPlots(g,2);
+                    showOnlyLineFlag = 1;
+                    lineStyle = '--';
+                    displayAndcompareData(hPSD,logPSDData{g,p},freqVals,displaySettings,yLimsPSD,0,useMedianFlag,~pairedDataFlag,showOnlyLineFlag,lineStyle);
+            end
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        else
+            hPSD = hAllPlots(g,i);
+            displayAndcompareData(hPSD,logPSDData{g,i},freqVals,displaySettings,yLimsPSD,1,useMedianFlag,~pairedDataFlag);
+            xlim(hPSD,freqLims);
 
-        yticks(hPSD,yLimsPSD(1):1:yLimsPSD(end));
+            % Add lines in PSD plots
+            for k=1:2
+                line([freqRangeList{freqPos}(k) freqRangeList{freqPos}(k)],yLimsPSD,'LineStyle','--','LineWidth',2,'color','k','parent',hPSD);
+            end
 
-        if g==2 && i==1
-            legend('','Meditators','','Controls','FontWeight','bold','FontSize',12);
-            legend('boxoff')
-            text(75,1.2,'p<0.05','Color','c','FontSize',fontsize+3,'FontWeight','bold');
-            text(75,0.8,'p<0.01','Color','k','FontSize',fontsize+3,'FontWeight','bold');
-        end
+            if ~strcmp(refChoices{1},'none')
+                line([0 freqVals(end)],[0 0],'color','k','parent',hPSD);
+            end
 
-        %%%%%%%%%%%%%%%%%% Plot topoplot %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        if i==3
-            if plotTopoFlag
-                for s=1:2 %  meditators  and control
-                    axes(hTopo(s));
-                    title(groupNames{s});
-                    data = logTopoData{i,1}{s};                                                                                                                                                   data = logTopoData{i}{s};
-                    topoplot(data,montageChanlocs,'electrodes','on','maplimits',cLimsTopo,'plotrad',0.6,'headrad',0.6);
-                    if i==2 && s==1
-                        ach=colorbar;
-                        ach.Location='southoutside';
-                        ach.Position =  [ach.Position(1) ach.Position(2)-0.05 ach.Position(3) ach.Position(4)];
-                        ach.Label.String = '\Delta Power (dB)';
-                    end
+            yticks(hPSD,yLimsPSD(1):1:yLimsPSD(end));
 
-                    if s==1
-                        [electrodeGroupList,groupNameList] = getElectrodeGroups(gridType,capType);
-                        numGroups = length(electrodeGroupList);
-                        for n=1:numGroups
-                            showElecIDs = electrodeGroupList{n};
-                            topoplot_murty([],montageChanlocs,'electrodes','on','style','blank','drawaxis','off','nosedir','+X','emarkercolors',x,'plotchans',showElecIDs,'plotrad',0.65,'headrad',0.6,'emarker',{'.',colorList(n,:),14,1},'plotrad',0.6,'headrad',0.6);
+            if g==2 && i==1
+                legend('','Meditators','','Controls','FontWeight','bold','FontSize',12);
+                legend('boxoff')
+                text(75,1.2,'p<0.05','Color','c','FontSize',fontsize+3,'FontWeight','bold');
+                text(75,0.8,'p<0.01','Color','k','FontSize',fontsize+3,'FontWeight','bold');
+            end
+
+            %%%%%%%%%%%%%%%%%% Plot topoplot %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            if i==3
+                if plotTopoFlag
+                    for s=1:2 %  meditators  and control
+                        axes(hTopo(s));
+                        title(groupNames{s});
+                        data = logTopoData{i,1}{s};                                                                                                                                                   data = logTopoData{i}{s};
+                        topoplot(data,montageChanlocs,'electrodes','on','maplimits',cLimsTopo,'plotrad',0.6,'headrad',0.6);
+                        if i==2 && s==1
+                            ach=colorbar;
+                            ach.Location='southoutside';
+                            ach.Position =  [ach.Position(1) ach.Position(2)-0.05 ach.Position(3) ach.Position(4)];
+                            ach.Label.String = '\Delta Power (dB)';
+                        end
+
+                        if s==1
+                            [electrodeGroupList,groupNameList] = getElectrodeGroups(gridType,capType);
+                            numGroups = length(electrodeGroupList);
+                            % for n=1:numGroups
+                            %     showElecIDs = electrodeGroupList{n};
+                            %     topoplot_murty([],montageChanlocs,'electrodes','on','style','blank','drawaxis','off','nosedir','+X','emarkercolors',x,'plotchans',showElecIDs,'plotrad',0.65,'headrad',0.6,'emarker',{'.',colorList(n,:),14,1},'plotrad',0.6,'headrad',0.6);
+                            % end
                         end
                     end
+
                 end
-
             end
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         end
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
     end
 end
 
